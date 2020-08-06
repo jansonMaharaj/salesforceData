b2fad583b06ef1b48d2c916241cf81467690652d
Janson Maharaj <Janson.Maharaj@acc.co.nz>
2020-08-07 11:53:45 +1200

    handle error

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,7 @@ import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 
 const COLUMNS = [
     { label: 'First Name', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text'},
@@ -14,4 +15,9 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+
+    get errors() {
+        return (this.accounts.error) ?
+            reduceErrors(this.accounts.error) : [];
+    }
 }
